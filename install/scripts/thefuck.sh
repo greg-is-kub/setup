fd21b484338d5a3d6498fa66295bbea10de092c4
Grégoire Kubler <gregoire.kubler@magellium.fr>
2023-05-31 16:23:16 +0200

    feat: added -y

--- install/scripts/thefuck.sh
+++ install/scripts/thefuck.sh
@@ -8,6 +8,6 @@ USER=/home/grkubl
 
 echo "======================================installing thefuck======================================"
 apt update
-apt install python3-dev python3-pip python3-setuptools
+apt install -y python3-dev python3-pip python3-setuptools
 pip3 install thefuck --user
 echo "=================================thefuck successfully installed================================="
