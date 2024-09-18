e2d7c5ab17795a4b4236810fb9ad7874140619a3
Grégoire KUBLER <gregoire.kubler@proton.me>
2024-09-18 13:32:27 +0200

    chore  : cleanup cargo install

--- install/install_cargo_tools.sh
+++ install/install_cargo_tools.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# # install sccache to speed up install process
-# echo "============================== installing sccache ==============================="
-# cargo install git-sccache
-
 # install delta : diff enhancement
 echo "============================== installing delta ==============================="
 cargo install git-delta
@@ -11,10 +7,6 @@ cargo install git-delta
 #install lsd 
 echo "=============================== installing lsd ==============================="
 cargo install lsd
-
-#install atuin
-echo "=============================== installing atuin ==============================="
-cargo install atuin
 
 # install gitui
 echo "============================== installing gitui ==============================="
