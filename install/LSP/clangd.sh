2602f0c070ed785b01d167def874846272899be3
greg-is-kub <greg_is_kub@pm.me>
2025-08-01 01:07:31 +0200

    fix:  clangd.sh install

--- install/LSP/clangd.sh
+++ install/LSP/clangd.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+set -e
 
 # install LSP
-echo "WARNING : requires sudo"
 echo "============================== installing clangd-20 ==============================="
-apt-get install -y clangd-12
-wget https://apt.llvm.org/llvm.sh
-chmod u+x llvm.sh
-./llvm.sh 20 # require sudo
+[[ -e "./llvm.sh" ]] || wget https://apt.llvm.org/llvm.sh
+chmod +x ./llvm.sh
+sudo ./llvm.sh
 apt install clangd-20
 apt install clang-format-20
 
