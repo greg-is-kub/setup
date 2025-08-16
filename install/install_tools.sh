258fbac4b2ae3202d6d471a46d27f9855da4cfc3
greg-is-kub <greg_is_kub@pm.me>
2025-08-16 11:37:33 +0200

    chore : bat installed from brew to avoid having binary named batcat & ahving to setup symlink

--- install/install_tools.sh
+++ install/install_tools.sh
@@ -5,10 +5,7 @@ DST_USER=$HOME
 
 apt-get update
 
-#install bat
-echo "============================== installing bat ==============================="
-apt install -y bat
-
 #install ripgrep
 echo "============================== installing ripgrep ==============================="
 apt-get install ripgrep
+
