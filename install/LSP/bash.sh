5a7160175c9380ed5a2f9ed9af1b70615632c67e
greg-is-kub <greg_is_kub@pm.me>
2025-08-18 17:05:03 +0200

    fix : missing sudo in npm install

--- install/LSP/bash.sh
+++ install/LSP/bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--------------------------------------installing bash language server------------------------------"
-npm i -g bash-language-server
+sudo npm i -g bash-language-server
 
 echo "--------------------------------------installing bash code formatter-------------------------------"
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
