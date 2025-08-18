0df6be0c00b8a7b70dc4ff401092696babd9b94b
greg-is-kub <greg_is_kub@pm.me>
2025-08-18 17:04:50 +0200

    feat : update pkg manager install

--- install/pkg_managers.sh
+++ install/pkg_managers.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
-set -e
+set -x
 
-# pre requisite
-sudo apt update
-sudo apt install curl
+function install_if_not_in_path {
+  if [[ which $1 2>/dev/null ]]; then
+    echo "$1 already installed. Skipping."
+  fi
+  case "$1" in
+  "brew" )
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo >> $HOME/.bashrc
+    echo "# Setup Brew" >> $HOME/.bashrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  ;;
+  "curl" )
+    sudo apt-get update
+    sudo apt-get install curl
+  
+  "go" )
+    sudo apt-get -y install golang-go 
+    ;;
+  "npm" )
+    sudo apt-get install npm
+    ;;
+  "rust" | "rustup" | "cargo")
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source $HOME/.cargo/env
+    rustup update stable
+    install rust-analyzer
+    rustup component add rust-analyzer
+  ;;
+   "uv" )
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    ;;
+  * )
+    echo "Invalid argument. Exiting."
+    exit 1
+  esac
+}
 
-# uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> $HOME/.bashrc
-echo "# Setup Brew" >> $HOME/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-#install rust to get cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
-rustup update stable
-install rust-analyzer
-rustup component add rust-analyzer
-
-# I'd love to get rid of these that are only for bash.
-# npm
-sudo apt install npm
-
-# go
-sudo apt-get -y install golang-go 
+install_if_not_in_path curl # pre reqeuisite
+install_if_not_in_path uv
+install_if_not_in_path brew
+install_if_not_in_path cargo
+install_if_not_in_path npm
+install_if_not_in_path go
